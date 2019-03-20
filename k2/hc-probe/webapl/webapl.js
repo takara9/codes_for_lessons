54708b3d0b5e0c46e03bc481b6b0a0c9f17568eb
Maho Takara <takara9@gmail.com>
2019-03-20 07:07:04 +0000

    change comment mark

--- k2/hc-probe/webapl/webapl.js
+++ k2/hc-probe/webapl/webapl.js
@@ -1,14 +1,14 @@
-## 模擬アプリケーション
-#
+// 模擬アプリケーション
+//
 const express = require('express')
 const app = express()
 var start = Date.now()
 
-## Livenessプローブのハンドラー
-## 模擬障害として、起動から40秒を超えると、HTTP 500内部エラーを返します。
-## 40秒までは、HTTP 200 OKを返します。
-## つまり、40秒を超えると、Livenessプローブが失敗して、コンテナが再起動します。
-#
+// Livenessプローブのハンドラー
+// 模擬障害として、起動から40秒を超えると、HTTP 500内部エラーを返します。
+// 40秒までは、HTTP 200 OKを返します。
+// つまり、40秒を超えると、Livenessプローブが失敗して、コンテナが再起動します。
+//
 app.get('/healthz', function(request, response) {
     var msec = Date.now() - start
     var code = 200
@@ -19,11 +19,10 @@ app.get('/healthz', function(request, response) {
     response.status(code).send('OK')
 })
 
-## Redinessプローブのハンドラー
-## アプリケーションの初期化をシミュレーションして、
-## 起動してから20秒経過後、HTTP 200を返します。 
-## それまでは、HTTPS 200 OKを返します。
-#
+// Redinessプローブのハンドラー
+// アプリケーションの初期化をシミュレーションして、
+// 起動してから20秒経過後、HTTP 200を返します。 
+// それまでは、HTTPS 200 OKを返します。
 app.get('/ready', function(request, response) {
     var msec = Date.now() - start
     var code = 500
@@ -34,13 +33,13 @@ app.get('/ready', function(request, response) {
     response.status(code).send('OK')
 })
 
-## トップページ
-#
+// トップページ
+//
 app.get('/', function(request, response) {
     console.log('GET /')
     response.send('Hello from Node.js')
 })
 
-## サーバーポート番号 TCP
-#
+// サーバーポート番号 TCP
+//
 app.listen(3000);
